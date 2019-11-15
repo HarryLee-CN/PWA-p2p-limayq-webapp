09663719bf29b581f9d85b6b74ab4d3de530452e
李秋奇 <mail@qiuqi.li>
2019-11-15 15:52:54 +0800

    update

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,5 +1,5 @@
 export default {
   install(Vue) {
-    Vue.prototype.$getApi = process.env.NODE_ENV === 'development' ? '/devApi' : '';
+    Vue.prototype.$getApi = process.env.NODE_ENV === 'development' ? '/devApi' : 'https://p2p.liqiuqi.cn';
   }
 }
