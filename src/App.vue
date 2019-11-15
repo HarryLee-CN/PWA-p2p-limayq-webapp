06ed6c598070735373e41529172a0d88ba643b56
李秋奇 <mail@qiuqi.li>
2019-11-15 19:21:43 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" style="overflow: hidden;">
     <transition name="fade" mode="out-in">
       <keep-alive>
         <router-view v-if="$route.meta.keep"></router-view>
